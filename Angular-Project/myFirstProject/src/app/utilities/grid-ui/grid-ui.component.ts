c4f3684ae8169d2c160b609b8ebcbb5a6028327b
Sourav banerjee <sourav.banerjee116@gmail.com>
2022-09-13 01:59:45 +0530

    Common Module Added

--- Angular-Project/myFirstProject/src/app/utilities/grid-ui/grid-ui.component.ts
+++ Angular-Project/myFirstProject/src/app/utilities/grid-ui/grid-ui.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-grid-ui',
@@ -6,9 +6,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GridUiComponent implements OnInit {
 
+  gridColumn:Array<any> = new Array<any>();
+  gridData:Array<any> = new Array<any>();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @Input("grid-columns")
+  set SetGridColums(_gridcolumn:Array<any>){
+    this.gridColumn=_gridcolumn;
+  }
+  @Input("grid-data")
+  set SetGridData(_griddata:Array<any>){
+    this.gridData=_griddata;
   }
 
 }
