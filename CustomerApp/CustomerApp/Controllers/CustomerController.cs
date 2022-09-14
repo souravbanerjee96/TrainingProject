75b7d9c03c699c46cbf3b693b8ac82b40c1fdc44
Sourav banerjee <sourav.banerjee116@gmail.com>
2022-09-15 01:47:20 +0530

    Backend created WebAPI MVC

---
+++ CustomerApp/CustomerApp/Controllers/CustomerController.cs
@@ -0,0 +1,62 @@
+﻿using CustomerApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        CustomerDBContext db = new CustomerDBContext();
+        [HttpGet]
+        public IEnumerable<Customer> Get()
+        {
+            return db.Customers;
+        }
+
+        [HttpPost]
+        public IActionResult Post(Customer customer)
+        {
+            db.Customers.Add(customer);
+            db.SaveChanges();
+            var successmessage = new { Status = "Success" };
+            return Ok(successmessage);
+        }
+        [HttpPut("{id:int}")]
+        public IActionResult Put(int id , Customer customer)
+        {
+            var successmessage=new { Status = "Failed !!"};
+            var data = db.Customers.Where(x => x.Id == id).SingleOrDefault();
+            if (data != null)
+            {
+                data.CustomerCode = customer.CustomerCode;
+                data.CustomerName = customer.CustomerName;
+                data.CustomerSalary = customer.CustomerSalary;
+                db.SaveChanges();
+                successmessage = new { Status = "Success" };
+            }
+            else { }
+            
+            return Ok(successmessage);
+        }
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete(int id)
+        {
+            var successmessage = new { Status = "Failed !!" };
+            var data = db.Customers.Where(x => x.Id == id).SingleOrDefault();
+            if (data != null)
+            {
+                db.Customers.Remove(data);
+                db.SaveChanges();
+                successmessage = new { Status = "Success" };
+            }
+            return Ok(successmessage);
+        }
+
+    }
+}
---
+++ CustomerApp/CustomerApp/Models/CustomerDBContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace CustomerApp.Models
+{
+    public partial class CustomerDBContext : DbContext
+    {
+        public CustomerDBContext()
+        {
+        }
+
+        public CustomerDBContext(DbContextOptions<CustomerDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=CTSDOTNET858;Initial Catalog=CustomerDB;User ID=sa;Password=pass@word1");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.ToTable("Customer");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.CustomerCode).HasMaxLength(50);
+
+                entity.Property(e => e.CustomerName).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
