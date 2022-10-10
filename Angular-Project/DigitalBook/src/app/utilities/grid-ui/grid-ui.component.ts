8a445c15c13468b49f8d2931968a4c057d7f5210
Sourav banerjee <sourav.banerjee116@gmail.com>
2022-10-11 02:39:01 +0530

    added

--- Angular-Project/DigitalBook/src/app/utilities/grid-ui/grid-ui.component.ts
+++ Angular-Project/DigitalBook/src/app/utilities/grid-ui/grid-ui.component.ts
@@ -14,7 +14,7 @@ export class GridUiComponent implements OnInit {
   private _baseURL = GlobalVariable.BASE_API_URL + 'Reader';
   public _imgURL = GlobalVariable.IMG_URL;
   Readmsg: string = '';
-  email_reg = /[a-z0-9!#$%&'*+=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+  email_reg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   gridColumn: Array<any> = new Array<any>();
   gridBook: Array<any> = new Array<any>();
   gridData: Array<any> = new Array<any>();
